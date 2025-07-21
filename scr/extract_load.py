a566e215d4b52d0ad974ab1f09753c03277da8a7
Gustavo Onaga <gustavo.onaga1@gmail.com>
2025-07-21 15:30:35 -0300

    teste

---
+++ scr/extract_load.py
@@ -0,0 +1,26 @@
+# import
+import yfinance as yf
+import pandas as pd
+from alchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+
+# import das minhas variaveis de ambiente
+
+commodities = ['CL = F','CG = F','SI = F']
+
+def buscar_dados_commodities(simbolo, periodo = '5d',intervalo = '1d')
+    ticker = yf.Ticker('CL=F')
+    
+    
+
+    
+
+
+# pegar a cotacao dos meus ativos
+
+# concatenar os meus ativos (1..2..3) -> (1)
+
+
+# Salavar no banco de dados
